e7cdafb6c6dc3921c1c3a1860e70ba145dafb9c0
Ethan <ethan.onecolucci@gmail.com>
2026-05-03 09:18:59 +0100

    refactor code+grass move

---
+++ fluffyBird/background.py
@@ -0,0 +1,65 @@
+import pygame
+import config
+from dataclasses import dataclass
+
+
+@dataclass
+class Background:
+
+    def init(self):
+        self.clouds = pygame.image.load(
+            "fluffyBird/assets/images/Clouds.png"
+        ).convert_alpha()
+        self.grass = pygame.image.load(
+            "fluffyBird/assets/images/Grass.png"
+        ).convert_alpha()
+
+        self.sky_rect = (0, 0, config.SCREEN_WIDTH, self.clouds.get_height())
+        self.sea_rect = (
+            0,
+            self.sky_rect[3],
+            config.SCREEN_WIDTH,
+            config.SCREEN_HEIGHT - self.sky_rect[3],
+        )
+
+        self.clouds_arr = []
+        self.grass_arr = []
+
+        self._initSprites(self.clouds, self.clouds_arr, 0)
+        self._initSprites(
+            self.grass, self.grass_arr, config.SCREEN_HEIGHT - self.grass.get_height()
+        )
+
+    def _initSprites(
+        self, image: pygame.Surface, images_arr: list[pygame.Rect], y: int
+    ):
+        for i in range(0, config.BG_REPEAT):
+            rect = image.get_rect()
+            rect.topleft = (i * rect.width, y)
+            images_arr.append(rect)
+
+    def _drawSprites(
+        self,
+        screen: pygame.Surface,
+        image: pygame.Surface,
+        images_arr: list[pygame.Rect],
+    ):
+        if images_arr[0].right <= 0:
+            first_rect = images_arr.pop(0)
+
+            new_rect = image.get_rect()
+            new_rect.topleft = (config.SCREEN_WIDTH, first_rect.y)
+
+            images_arr.append(new_rect)
+
+        for rect in images_arr:
+            rect.x -= config.BG_SPEED
+            screen.blit(image, rect)
+
+    def draw(self, screen: pygame.Surface):
+
+        pygame.draw.rect(screen, (30, 140, 184), self.sky_rect)
+        pygame.draw.rect(screen, (4, 112, 236), self.sea_rect)
+
+        self._drawSprites(screen, self.clouds, self.clouds_arr)
+        self._drawSprites(screen, self.grass, self.grass_arr)
